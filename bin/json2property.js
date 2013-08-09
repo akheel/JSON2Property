d175e9e631e022c480ef9a75913a15dad3f4f153
James Black <james@James-Black.com>
2013-08-09 15:45:02 -0600

    Changing the command, marked as alpha 1.0

--- bin/json2property.js
+++ bin/json2property.js
@@ -5,7 +5,7 @@ var JSON2Property = require('../lib/json2property');
 var program = require('commander');
 
 program
-  .version('0.1.0')
+  .version('1.0.0-alpha')
   .option('-t, --test', 'lol test option')
   .parse(process.argv);
 
