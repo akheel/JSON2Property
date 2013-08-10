bc0710c0ae0fc3e5550ac86fba22e3c55f6aba16
James Black <james@James-Black.com>
2013-08-09 18:47:07 -0600

    Preping Alpha Version 1, doing a large rehaul of the CLI aspect

--- bin/json2property.js
+++ bin/json2property.js
@@ -1,12 +1,64 @@
 #!/usr/bin/env node
 
-var JSON2Property = require('../lib/json2property');
 
+var json2property = require('../lib/json2property');
+var glob = require('glob');
+var write = require('fs').writeFile;
+var read = require('fs').readFile;
+var ProgressBar = require('progress');
 var program = require('commander');
+var Batch = require('batch');
+var batch = new Batch();
+var mkpath = require('mkpath');
 
 program
   .version('1.0.0-alpha')
-  .option('-t, --test', 'lol test option')
+  .option('-i, --in <directory>', 'input directory', process.cwd())
+  .option('-o, --out <directory>', 'output directory', process.cwd())
+  .option('-p, --pattern <pattern>', 'pattern to match (uses glob)', '/**/*.json')
   .parse(process.argv);
 
-new JSON2Property(program.args).read_directory();
+
+glob(program.in + "" + program.pattern, function(error, files){
+  var i;
+  var read_file;
+  var path, file_name, last_slash;
+
+  console.log(files);
+
+  if (error) { throw_error(error); }
+  files = files.map(function(file){
+    last_slash = file.lastIndexOf("/");
+    path = file.slice(0, last_slash + 1);
+    file_name = file.slice(last_slash + 1);
+    if (last_slash === -1) {
+      path = "";
+      file_name = file;
+    }
+    return { path: path, file_name: file_name };
+  });
+
+  var bar = new ProgressBar("Transcoding item :current of :total [:bar] :percent, :etas",
+    {
+      total: files.length,
+      complete: '=',
+      incomplete: '-',
+      width: 40
+  });
+
+
+});
+
+var process_files = function(error, files) {
+  if (error) { throw_error(error); }
+
+  console.log("process_files = ", files);
+};
+
+var throw_error = function(error) {
+  console.log(error);
+  process.exit(1);
+};
+
+
+// new JSON2Property(program.args).read_directory();
